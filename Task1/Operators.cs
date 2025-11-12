49767fd7ec6863dff9d7943ff5859d544f7f7b7c
Anish-Programmer <at7anish@gmail.com>
2025-11-12 11:46:07 +0545

    Task 1 completed

---
+++ Task1/Operators.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Operators
+    {
+
+        // function to add two numbers 
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        // function to subtract two numbers 
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+
+        // function to multiply two numbers
+        public int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+
+        // function to divide two numbers
+        public int Divide(int a, int b)
+        {
+            return a / b;
+        }
+
+
+        // oddEvenFinder function
+        public void OddEvenFinder(int number)
+        {
+            string result = (number % 2 == 0) ? "Even Number" : "Odd Number";
+
+            Console.WriteLine($"{number} is {result}");
+        }
+
+    }
+}
--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace Task1
+﻿using System.ComponentModel;
+
+namespace Task1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // creating two integer variables
+           // int a = 20;
+            // int b = 10;
+
+                
+            // taking input from users
+            Console.WriteLine("Enter first number:");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+
+            // creating objects of Operators class
+            Operators op = new Operators();
+
+
+            // calling add function
+            int sum = op.Add(a, b);
+
+            // calling subtract function    
+            int difference = op.Subtract(a, b);
+
+            // calling multiply function
+            int product = op.Multiply(a, b);
+
+            // calling divide function
+            int quotient = op.Divide(a, b);
+
+
+            // displaying results
+            Console.WriteLine("Addition: " + sum);
+            Console.WriteLine("Subtraction: " + difference);
+            Console.WriteLine("Multiplication: "+ product);
+            Console.WriteLine("Division: " + quotient);
+
+            
+            // calling OddEvenFinder function
+            op.OddEvenFinder(a);
         }
+
     }
 }

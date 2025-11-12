d2204cbf55ddc2954e277204bed88bd40079ed0d
Anish-Programmer <at7anish@gmail.com>
2025-11-12 12:49:15 +0545

    Task 3 completed

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -4,7 +4,23 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // 1. asking user input
+            Console.Write("Enter your age: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            if (age < 13)
+            {
+                Console.WriteLine("Child");
+            }
+            else if (age>13 && age <19 ) 
+            {
+                Console.WriteLine("Teenager");
+            }
+            else if (age >= 20)
+            {
+                Console.WriteLine("Adult");
+            }
+
         }
     }
 }
